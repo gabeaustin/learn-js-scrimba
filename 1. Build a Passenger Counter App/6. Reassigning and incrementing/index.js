a8a5ad5d773aa4a2bc286ca39f435a1d6cb75554
gabeaustin <gabriel.austin2019@gmail.com>
2021-09-19 23:44:51 -0500

    started working on the user view - the html

--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -1,10 +1,17 @@
 // document.getElementById("count").innerText = 5
 
-let count = 5
+let count = 5;
 
-console.log(count)
+count = 3;
+
+console.log(count);
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints); // 100
+
 // Decrease it down to 25, and then finally increase it to 70
-
-
+bonusPoints = bonusPoints - 75;
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints); // 70
